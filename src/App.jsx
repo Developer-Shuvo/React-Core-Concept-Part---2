9f60001546dd6ca70155efdeb54e73e2febf9e76
SHUVO <im.computer.engineeer@gamil.com>
2025-07-01 22:31:11 +0600

    Button Style added

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,42 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-
-  function handleClick (){
-    alert ("Button Clicked!");
+  // Normal function to handle button click
+  function handleClick() {
+    alert("Button Clicked!");
   }
+
+  // Arrow function to handle button click
+  const handleClick2 = () => {
+    alert("Button Clicked with Arrow Function!");
+  };
+
+  const buttonStyle = {
+    marginRight: "16px",
+    padding: "10px 20px",
+    backgroundColor: "yellow",
+    color: "black",
+    border: "2px solid green",
+    borderRadius: "4px",
+    cursor: "pointer",
+    fontSize: "16px",
+  };
 
   return (
     <>
       <h2>React Core Concept Part-2</h2>
-      <button onClick={handleClick}>Click Me</button>
+
+      <div>
+        <button style={buttonStyle} onClick={handleClick}>
+          Normal Function Button
+        </button>
+        <button
+          style={{ ...buttonStyle, marginRight: 0 }}
+          onClick={handleClick2}
+        >
+          Arrow Function Button
+        </button>
+      </div>
     </>
   );
 }
